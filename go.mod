ee817a3b0db0fbf7b78571775717329b77805610
chizidotdev <chizivwokoma@gmail.com>
2022-12-28 23:44:49 +0100

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/chizidotdev/golang-graphql
+
+go 1.18
+
+require github.com/graphql-go/graphql v0.8.0 // indirect
---
+++ main.go
@@ -0,0 +1,158 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/graphql-go/graphql"
+)
+
+type Tutorial struct {
+	ID       int
+	Title    string
+	Author   Author
+	Comments []Comment
+}
+
+type Author struct {
+	Name      string
+	Tutorials []int
+}
+
+type Comment struct {
+	Body string
+}
+
+func populate() []Tutorial {
+	author := &Author{
+		Name:      "Chizi Victor",
+		Tutorials: []int{1},
+	}
+	tutorial := Tutorial{
+		ID:     1,
+		Title:  "Go graphQL Tutorial",
+		Author: *author,
+		Comments: []Comment{
+			{Body: "First Comment"},
+		},
+	}
+
+	var tutorials []Tutorial
+	tutorials = append(tutorials, tutorial)
+
+	return tutorials
+}
+
+func main() {
+	fmt.Println("GraphQL Golang")
+	tutorials := populate()
+
+	var commentType = graphql.NewObject(
+		graphql.ObjectConfig{
+			Name: "Comment",
+			Fields: graphql.Fields{
+				"body": &graphql.Field{
+					Type: graphql.String,
+				},
+			},
+		},
+	)
+
+	var authorType = graphql.NewObject(
+		graphql.ObjectConfig{
+			Name: "Author",
+			Fields: graphql.Fields{
+				"Name": &graphql.Field{
+					Type: graphql.String,
+				},
+				"Tutorials": &graphql.Field{
+					Type: graphql.NewList(graphql.Int),
+				},
+			},
+		},
+	)
+
+	var tutorialType = graphql.NewObject(
+		graphql.ObjectConfig{
+			Name: "Tutorial",
+			Fields: graphql.Fields{
+				"id": &graphql.Field{
+					Type: graphql.Int,
+				},
+				"title": &graphql.Field{
+					Type: graphql.String,
+				},
+				"author": &graphql.Field{
+					Type: authorType,
+				},
+				"comments": &graphql.Field{
+					Type: graphql.NewList(commentType),
+				},
+			},
+		},
+	)
+
+	fields := graphql.Fields{
+		"tutorial": &graphql.Field{
+			Type:        tutorialType,
+			Description: "Get Tutorial by ID",
+			Args: graphql.FieldConfigArgument{
+				"id": &graphql.ArgumentConfig{
+					Type: graphql.Int,
+				},
+			},
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				id, ok := p.Args["id"].(int)
+				if ok {
+					for _, tutorial := range tutorials {
+						if int(tutorial.ID) == id {
+							return tutorial, nil
+						}
+					}
+				}
+				return nil, nil
+			},
+		},
+		"list": &graphql.Field{
+			Type:        graphql.NewList(tutorialType),
+			Description: "Get Full Tutorial List",
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				return tutorials, nil
+			},
+		},
+	}
+
+	rootQuery := graphql.ObjectConfig{
+		Name:   "RootQuery",
+		Fields: fields,
+	}
+	schemaConfig := graphql.SchemaConfig{
+		Query: graphql.NewObject(rootQuery),
+	}
+	schema, err := graphql.NewSchema(schemaConfig)
+	if err != nil {
+		log.Fatalf("Failed to create new Schema, err %v", err)
+	}
+
+	query := `
+		{
+			list {
+				id
+				title
+			}
+		}
+	`
+
+	params := graphql.Params{
+		Schema:        schema,
+		RequestString: query,
+	}
+	r := graphql.Do(params)
+	if len(r.Errors) > 0 {
+		log.Fatalf("Failed to execute graphql operation, errors: %+v", r.Errors)
+	}
+
+	rJSON, _ := json.Marshal(r)
+	fmt.Printf("%s \n", rJSON)
+}
